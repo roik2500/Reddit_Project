1b0653d2b2d6f0eddfcd9b3bbae19b61cce3fd18
roi <roikreme@post.bgu.ac.il>
2021-09-17 17:27:03 +0300

    Roi - update sentiment and anylasis

--- statistics_analysis/Statistics.py
+++ statistics_analysis/Statistics.py
@@ -1,10 +1,13 @@
 
+import matplotlib.pyplot as plt
 
 class Statistic:
-    def __init__(self):
+    def __init__(self,subreddit,type):
         self.num_of_img = 0
         self.num_of_video = 0
         self.numOfPosts = 0
+        self.subreddit = subreddit
+        self.type_of_post = type
 
     '''
     This function is called from the main loop in Main_test file
@@ -14,39 +17,80 @@ class Statistic:
     :argument post: specific post from db 
     '''
     def precentage_media(self,con,post):
-        # create a new object of connection to DB
-        #con = Con_DB()
-        #posts = con.get_cursor_from_mongodb(collection_name="politics")
+        pushift_keys={}
+        if self.type_of_post == "Removed":
+            if post['reddit_api']['post']['selftext'] == "[removed]":
+                self.numOfPosts += 1  # counting the number of posts
+                pushift_keys = post['pushift_api'].keys()
+            else:
+                return
 
-        # # Taking the posts that contains only variable "is_video"
-        # all_posts = con.get_posts_text(posts, "is_video")
-       # numOfVideo, numOfImg, numOfPosts = 0, 0, 0
+        elif self.type_of_post == "NotRemoved":
+            if post['reddit_api']['post']['selftext'] != "[removed]":
+                self.numOfPosts += 1  # counting the number of posts
+                pushift_keys = post['pushift_api'].keys()
+            else:
+                return
 
-        #for post in tqdm(posts.find({})):
+        elif self.type_of_post == "All":
             self.numOfPosts += 1  # counting the number of posts
             pushift_keys = post['pushift_api'].keys()
+        else:
+            return
 
-            if 'preview' in pushift_keys:
-                # checking if this post contains a image
-                if post['pushift_api']["preview"] is not None and post['pushift_api']["preview"]['enabled'] == True:
-                    self.num_of_img += 1
+        if 'preview' in pushift_keys:
+            # checking if this post contains a image
+            if post['pushift_api']["preview"] is not None and post['pushift_api']["preview"]['enabled'] == True:
+                self.num_of_img += 1
 
-            if 'is_video' in pushift_keys:
-                # checking if this post contains a video
-                if post['pushift_api']["is_video"]:
-                    self.num_of_video += 1
+        if 'is_video' in pushift_keys:
+            # checking if this post contains a video
+            if post['pushift_api']["is_video"]:
+                self.num_of_video += 1
+
+    def draw_statistic_bars(self, sentimentObject):
+        x = list(sentimentObject.text_per_month.keys())
+        k = [val['positive'] for val in sentimentObject.sentiment_per_month.values()]
+        y = [val['neutral'] for val in sentimentObject.sentiment_per_month.values()]
+        z = [val['negative'] for val in sentimentObject.sentiment_per_month.values()]
+
+        x1 = [i+0.27 for i in list(sentimentObject.text_per_month.keys())]
+        x2 = [i+(0.27*2) for i in list(sentimentObject.text_per_month.keys())]
+        #plt.figure(figsize=(15, 5))
+
+        fig = plt.figure(figsize=(15, 5))
+        ax = fig.add_subplot(111)
+
+        rects1 = ax.bar(x, z, width=0.27, color='g', align='center')
+        rects2 = ax.bar(x1 , y, width=0.27, color='b', align='center')
+        rects3 = ax.bar(x2 , k, width=0.27, color='r', align='center')
+        ax.legend((rects1[0], rects2[0], rects3[0]), ('negative', 'neutral', 'positive'))
+
+        self.autolabel(rects1,ax)
+        self.autolabel(rects2,ax)
+        self.autolabel(rects3,ax)
+        return plt.show()
+
+    def autolabel(self,rects,ax):
+        for rect in rects:
+            h = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * h, '%d' % int(h),
+                    ha='center', va='bottom')
+
+
+
+
 
     '''
     This function is display the data, its mean printing the self variables in %
     '''
     def get_percent(self):
-        print("Statistic:\n")
+        print("Statistic for {}:".format(self.type_of_post))
         print("Num of posts: {}".format(self.numOfPosts))
         print("Num of images: {}".format(self.num_of_img))
         print("Num of videos: {}".format(self.num_of_video))
-
         print("Video Precentage: " + str(round((self.num_of_video / self.numOfPosts) * 100, 4)) + "%")
-        print("Image Precentage: " + str(round((self.num_of_img / self.numOfPosts) * 100, 4)) + "%")
+        print("Image Precentage: " + str(round((self.num_of_img / self.numOfPosts) * 100, 4)) + "%\n")
         #print("sub-reddit: politics\n Total Posts: 5000\n Year: 2020")
         return {'num_of_video':self.num_of_video,'num_of_img':self.num_of_img,'numOfPosts':self.numOfPosts}
 
--- text_analysis/Sentiment.py
+++ text_analysis/Sentiment.py
@@ -5,22 +5,29 @@ import matplotlib.pyplot as plt
 from afinn import Afinn
 from tqdm import tqdm
 
+
 class Sentiment:
     def __init__(self):
         self.text_per_month = {}
+        self.sentiment_per_month = {}  ##{'1': {"positive":5,'netural:4,'negative:8}, '2': {"positive":7,'netural:4,'negative:8}}
         self.positive = 0
         self.negative = 0
         self.neutral = 0
         self.total_posts = 0
+        self.percent_positive = 0
+        self.percent_negative = 0
+        self.percent_neutral = 0
+        self.subreddit = subreddit
+        self.type_of_post = type
 
-    def clean_tweet(self,tweet):
+    def clean_tweet(self, tweet):
         '''
             Utility function to clean tweet text by removing links, special characters
             using simple regex statements.
             '''
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
-    def get_polarity(self,text):
+    def get_polarity(self, text):
         analysis = TextBlob(self.clean_tweet(text))
         return analysis.sentiment.polarity
 
@@ -33,15 +40,10 @@ class Sentiment:
     return if this text is positive/neutral/negative
     :argument text: text
     '''
+
     def get_post_sentiment(self, text):
         # create TextBlob object of passed tweet text
         analysis = TextBlob(self.clean_tweet(text))
-        # analysis.detect_language(to="en")
-        # print("Sentiment:")
-        # print("The polarity is: {}".format(analysis.sentiment.polarity))
-        # print("The subjectivity is: {}".format(analysis.sentiment.subjectivity))
-
-        # set sentiment
         if analysis.sentiment.polarity > 0:
             return 'positive'
             # return 1
@@ -52,7 +54,24 @@ class Sentiment:
             return 'negative'
             # return -1
 
+    def check_removed_or_notremoved(self, post):
+        if self.type_of_post == "Removed":
+            if post['reddit_api']['post']['selftext'] == "[removed]":
+                return post['pushift_api'].keys()
+            else:
+                return None
 
+        elif self.type_of_post == "NotRemoved":
+            if post['reddit_api']['post']['selftext'] != "[removed]":
+                return post['pushift_api'].keys()
+            else:
+                return None
+
+        elif self.type_of_post == "All":
+            return post['pushift_api'].keys()
+
+        else:
+            return None
 
     '''
     This function is called from the main loop in Main_test file
@@ -63,33 +82,22 @@ class Sentiment:
                               month - x_values = days of the posts
                               year - x_values = month of the posts
     '''
-    def update_sentiment_values(self, post,remove_all_NotRemove,Month_Or_Year):
+
+    def update_sentiment_values(self, post, Month_Or_Year):
         keys = {}
-        if remove_all_NotRemove == "Removed":
-            if post['reddit_api']['post']['selftext'] == "[removed]":
-                keys = post['pushift_api'].keys()
-                self.total_posts += 1
-            else: return
+        # checking if this specific post is removed or not, depends in what we choose as a type
 
-        elif remove_all_NotRemove == "NotRemoved":
-            if post['reddit_api']['post']['selftext'] != "[removed]":
-                keys = post['pushift_api'].keys()
-                self.total_posts += 1
-            else: return
-
-        elif remove_all_NotRemove == "All":
-            keys = post['pushift_api'].keys()
-            self.total_posts += 1
-
-        else: return
+        keys = self.check_removed_or_notremoved(post, self.type_of_post)
+        if keys == None:
+            return
+        self.total_posts += 1
 
         text = ""
-
         if 'selftext' in keys:
             text = post['pushift_api']['selftext']
             text += post['pushift_api']['title']
         else:
-        #if 'title' in keys:
+            # if 'title' in keys:
             text += post['pushift_api']['title']
 
         temp = self.get_post_sentiment(text)
@@ -108,12 +116,14 @@ class Sentiment:
             x_value = int(datetime.datetime.strptime(post['pushift_api']['created_utc'][0], "%Y-%m-%d").date().day)
 
         # creating a list of all the text(title) per month
+        sentiment_month = {"positive": 0, 'neutral': 0, "negative": 0}
         if x_value not in self.text_per_month.keys():
             self.text_per_month[x_value] = [text]
+            self.sentiment_per_month[x_value] = sentiment_month
+            self.sentiment_per_month[x_value][temp] = 1
         else:
             self.text_per_month[x_value].append(text)
-
-
+            self.sentiment_per_month[x_value][temp] += 1
 
     '''
     This function drawing a graph of the sentiment by the self variables.
@@ -121,73 +131,78 @@ class Sentiment:
     :argument kind_of_sentiment - could be polarity or subjectivity
     :argument subreddit - Name of the sub-reddit
     :argument type - indicate if this a removed post or not. (Removed/All/Not Removed)
+    :argument topic (optionally) - the defult value is "". otherwise if we creating graph for topic this value will be
+                      the number of the topic (0,1,2....)
+    :argument fullpath (optionally) - if we want to save an img of the graph this is the full path to drive.
     '''
-    def draw_sentiment_time(self,kind_of_sentiment,subreddit,type):
+
+    def draw_sentiment_time(self, kind_of_sentiment, subreddit, type, topic="", fullpath=""):
         # calculate the % of positive, negative and neutral
-        positive = (self.positive / self.total_posts) * 100
-        negative = (self.negative / self.total_posts) * 100
-        neutral = (self.neutral / self.total_posts) * 100
+        self.percent_positive = (self.positive / self.total_posts) * 100
+        self.percent_negative = (self.negative / self.total_posts) * 100
+        self.percent_neutral = (self.neutral / self.total_posts) * 100
         y_value = []
 
-        afn = Afinn()
+        # afn = Afinn()
         for text in tqdm(self.text_per_month.values()):
-            #scores = sum([afn.score(article) for article in text]) / self.total_posts
             if kind_of_sentiment == "polarity":
-                temp = self.get_polarity(text[0])
-                for article in text:
-                    t = self.get_polarity(article)
+                # temp = self.get_polarity(text[0])
                 scores = sum([self.get_polarity(article) for article in text]) / self.total_posts
 
             if kind_of_sentiment == "subjectivity":
                 scores = sum([self.get_subjectivity(article) for article in text]) / self.total_posts
-
             y_value.append(scores)
 
         self.text_per_month = dict(sorted(self.text_per_month.items(), key=lambda item: item[0]))
 
-        #print("text_per_month: {}".format(self.text_per_month))
-        #print("Sentiment\polarity: {}".format(y_value))
+        # creating graph serially
+        self.draw_sentiment_option1(y_value, list(self.text_per_month.keys()), kind_of_sentiment)
 
-        x = max(list(self.text_per_month.keys()))+1
-        y = max(y_value)
+    def draw_sentiment_option1(self, Yaxis, Xaxis, kind_of_sentiment, topic="", fullpath=""):
+        # position of text plot
+        x = max(Xaxis) + 0.5
+        y = max(Yaxis) / 2
 
-        plt.plot(list(self.text_per_month.keys()), y_value, label="Year: 2020")
-        plt.text(x,y,
-                 "Sub-Reddit: {}\n Type of posts: {}\n Total Post: {}\n Year: 2020\n Positive: {}%\n Negative: {}%\n Neutral: {}%".format(
-                     subreddit,type,self.total_posts,round(positive,2), round(negative,2), round(neutral,2)))
-        plt.xlabel("Month")
-        plt.ylabel("Sentiment ({})".format(kind_of_sentiment))
+        fig = plt.figure(figsize=(15, 5))
+        ax = fig.add_subplot(111)
+
+        # position of the max point
+        ymax = max(Yaxis)
+        xpos = Yaxis.index(ymax)
+        xmax = Xaxis[xpos]
+
+        # position of the min point
+        ymin = min(Yaxis)
+        xpos2 = Yaxis.index(ymin)
+        xmin = Xaxis[xpos2]
+
+        line, = ax.plot(list(self.text_per_month.keys()), Yaxis, label="Year: 2020")
+
+        ax.annotate('Max', xy=(xmax, ymax), xytext=(xmax, ymax), arrowprops=dict(facecolor='black'))
+        ax.annotate('Min', xy=(xmin, ymin), xytext=(xmin, ymin), arrowprops=dict(facecolor='black'))
+
+        # check if this drawing is for topic anaylasis
+        if topic != "":
+            plt.text(x, y,
+                     "Sub-Reddit: {}\n Type of posts: {}\n Total Post: {}\n Year: 2020\n Positive: {}%\n Negative: {}%\n Neutral: {}%\n Topic: {}".format(
+                         self.subreddit, self.type_of_post, self.total_posts, round(self.percent_positive, 2),
+                         round(self.percent_negative, 2), round(self.percent_neutral, 2),
+                         topic))
+        else:
+            plt.text(x, y,
+                     "Sub-Reddit: {}\n Type of posts: {}\n Total Post: {}\n Year: 2020\n Positive: {}%\n Negative: {}%\n Neutral: {}%".format(
+                         self.subreddit, self.type_of_post, self.total_posts, round(self.percent_positive, 2),
+                         round(self.percent_negative, 2), round(self.percent_neutral, 2)))
+
+        # plt.xlabel("Month")
+        # plt.ylabel("Sentiment ({})".format(kind_of_sentiment))
+
+        # case: saving a plot img if there is a fuul path to locaion of saving
+        fileName = "topic"  # change to your file name
+        if fullpath != "":
+            if topic != "":
+                plt.savefig('{}/{} topic{}({}).png'.format(fullpath, fileName, topic, kind_of_sentiment))
+            else:
+                plt.savefig('{}/{} ({}).png'.format(fullpath, fileName, kind_of_sentiment))
+
         return plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # # for option count
-        # hist = pd.DataFrame(data, columns=['month', 'Sentiment'])
-        # with sns.axes_style('white'):
-        #     # g = sns.factorplot("Year", data=hist, aspect=1.7, kind='count', hue='Sentiment', order=range(2017, 2023))
-        #     g = sns.factorplot("month", data=hist, aspect=1.7, kind='count', hue='Sentiment', order=range(2017, 2023))
-        # return plt.show()
-
-
-# if __name__ == '__main__':
-#     api = Sentiment()
-#     con = Con_DB()
-#     # post_csv = api.get_post_from_csv()
-#     # api.draw_sentiment_time(post_csv,'c')
-#     posts = con.get_cursor_from_mongodb(collection_name="politics")
-#     #text = con.get_posts_text(posts,"title")
-#     api.draw_sentiment_time(posts)
-#
